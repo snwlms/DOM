186ee25196eb05c85169f99fa6cd2e5e3512e81d
Santosh <snwlms@gmail.com>
2018-02-07 20:24:32 +0530

    Bunao Disturbed

--- js/list.js
+++ js/list.js
@@ -5,6 +5,10 @@ el.className = 'groceries__item groceries__item-cool';
 console.log(el.className);
 */
 
+/*
+
+// Selecting elemsnts using class names
+
 var elements = document.getElementsByClassName('groceries__item-hot');
 
 if (elements.length > 2) {
@@ -13,3 +17,36 @@ if (elements.length > 2) {
 }
 
 console.log(elements.length);
+*/
+
+/*
+// Selecting elemsnts using Tag names
+
+var elements = document.getElementsByTagName('li');
+
+if (elements.length > 0) {
+    var el = elements[0];
+    el.className = el.className.replace('hot','cool');
+}
+*/
+
+
+/*
+//Selecting elemsnts using CSS Selectors
+
+var el = document.querySelector('.groceries__item-hot');
+el.className = el.className.replace('hot','cool');
+console.log(el);
+
+var els = document.querySelectorAll('.groceries__item-hot')
+els[1].className = els[1].className.replace('hot','cool');
+console.log(els);
+*/
+
+var hotItems = document.querySelectorAll('li.groceries__item-hot');
+ console.log(hotItems.length);
+ if (hotItems.length > 0){
+     for (var i =0; i < hotItems.length; i++){
+         hotItems[i].className = hotItems[i].className.replace('hot','cool');
+     }
+ }
