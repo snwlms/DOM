9d1e0723bb7629ed33157d264e3ce8051915fa69
Santosh <snwlms@gmail.com>
2018-02-07 16:20:22 +0530

    Class Attribute

--- js/list.js
+++ js/list.js
@@ -1,4 +1,15 @@
+/*
 var el = document.getElementById('one');
 el.className = 'groceries__item groceries__item-cool';
 
 console.log(el.className);
+*/
+
+var elements = document.getElementsByClassName('groceries__item-hot');
+
+if (elements.length > 2) {
+    var el = elements[2];
+    el.className = el.className.replace('hot','cool');
+}
+
+console.log(elements.length);
